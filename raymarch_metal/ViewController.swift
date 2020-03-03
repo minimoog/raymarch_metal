c24652de645b4bca293b51e3a14ee26fde8194d5
Toni Jovanoski <minimoog77@gmail.com>
2020-03-04 00:48:36 +0100

    Compute pipeline setup [WIP]

--- raymarch_metal/ViewController.swift
+++ raymarch_metal/ViewController.swift
@@ -7,14 +7,46 @@
 //
 
 import UIKit
+import MetalKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, MTKViewDelegate {
+    var commandQueue: MTLCommandQueue? = nil
+    var startTime: Double = CACurrentMediaTime()
+    
+    @IBOutlet weak var mtkView: MTKView! {
+        didSet {
+            mtkView.delegate = self
+            mtkView.preferredFramesPerSecond = 60
+            mtkView.clearColor = MTLClearColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        mtkView.device = MTLCreateSystemDefaultDevice()
+        commandQueue = mtkView.device?.makeCommandQueue()
+        
     }
 
-
+    func loadShader() -> MTLComputePipelineState? {
+        let library = mtkView.device?.makeDefaultLibrary()
+        
+        if let computeShader = library?.makeFunction(name: "compute") {
+            return try? mtkView.device?.makeComputePipelineState(function: computeShader)
+        }
+        
+        return nil
+    }
+    
+    func draw(in view: MTKView) {
+        let currentTime = CACurrentMediaTime()
+        let offsetTime = currentTime - startTime
+    }
+    
+    func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
+        
+    }
 }
 
